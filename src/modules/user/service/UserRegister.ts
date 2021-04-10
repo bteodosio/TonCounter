84e94d810053ac4457e4dd097f986b4a9defcee0
bteodosio <teodosio.goncalves@gmail.com>
2021-04-10 19:03:26 -0300

    feat(user register): added password encode

--- src/modules/user/service/UserRegister.ts
+++ src/modules/user/service/UserRegister.ts
@@ -2,6 +2,7 @@ import { IUser } from '../dtos/IUser'
 import log4js from 'log4js'
 import { IUserRepository } from '../repositories/IUserRepository'
 import { ErrorHandler } from '@shared/common/ErrorHandler'
+import md5 from 'md5'
 
 export class UserRegister {
     private logger: log4js.Logger
@@ -15,6 +16,11 @@ export class UserRegister {
     public async run (user: IUser): Promise<void> {
       try {
         if (!user.emailAddress || user.emailAddress === '') throw new ErrorHandler(400, 'Email not provided', 'User Register')
+
+        if (!user.password || user.password === '') throw new ErrorHandler(400, 'Password not provided', 'User Register')
+
+        user.password = md5(`${user.password}_${user.emailAddress}`)
+
         const insertParans = {
           TableName: `${process.env.USER_TABLE}`,
           Item: { ...user }
