476e10cdb7f83d16bcbcbdd5e340ba3803889000
bteodosio <teodosio.goncalves@gmail.com>
2021-04-11 13:51:53 -0300

    feat(unit test): added password unit test and remove unused dotenv

--- __tests__/unit/user/CreateUser.spec.ts
+++ __tests__/unit/user/CreateUser.spec.ts
@@ -2,13 +2,14 @@ import { User } from '../../../src/modules/user/entities/User'
 import { UserRegister } from '../../../src/modules/user/service/UserRegister'
 import { UserGetter } from '../../../src/modules/user/service/UserGetter'
 import { MockDB } from './MockDB'
+import md5 from 'md5'
 
 describe('Unit test for register an user', () => {
   const mockDB = new MockDB()
   const insertUser = new UserRegister(mockDB)
   const getUser = new UserGetter(mockDB)
 
-  it('Should insert new user when email is provided', async (done) => {
+  it('Should insert new user when email and password is provided', async (done) => {
     const userToInsert = new User({
       firstName: 'Bruno',
       lastName: 'Teodosio',
@@ -35,6 +36,39 @@ describe('Unit test for register an user', () => {
       expect(err.message).toBe('Email not provided')
     }
 
+    done()
+  })
+
+  it('Should throw an error when password is not provided', async (done) => {
+    const userToInsert = new User()
+    userToInsert.emailAddress = 'emailtest@gmail.com'
+    try {
+      await insertUser.run(userToInsert)
+      expect(true).toBe(false)
+    } catch (err) {
+      expect(err.message).toBe('Password not provided')
+    }
+
+    done()
+  })
+
+  it('Should encode new user password when email and password is provided', async (done) => {
+    const userToInsert = new User({
+      firstName: 'Bruno',
+      lastName: 'Teodosio',
+      age: 30,
+      streetAddress: 'Rua Lídia Borba',
+      emailAddress: 'passwordtest@gmail.com',
+      password: 'testeSenha'
+    })
+
+    const encodedPassword = md5(`${userToInsert.password}_${userToInsert.emailAddress}`)
+
+    await insertUser.run(userToInsert)
+
+    const insertedUser = await getUser.run('passwordtest@gmail.com')
+
+    expect(encodedPassword).toBe(insertedUser.password)
     done()
   })
 })
